bff3463b0b489df39d2fefe70b962493839bdc87
Magnus Glantz <sudo@redhat.com>
2017-12-05 11:54:46 +0100

    Major update, will probably break everything

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  </head>
  <body>
- <?php echo 'App Version: 11.0. Pod IP: '; ?>
+ <?php echo 'App Version: 12.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
